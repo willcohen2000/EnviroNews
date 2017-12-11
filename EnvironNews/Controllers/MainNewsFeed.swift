9a2e6c50c3e83a1f7be87e8db035943bc3f85bce
willcohen2000 <willcohen2000@gmail.com>
2017-12-11 10:54:06 -0500

    Library updates

---
+++ EnvironNews/Controllers/MainNewsFeed.swift
@@ -0,0 +1,22 @@
+//
+//  MainNewsFeed.swift
+//  EnvironNews
+//
+//  Created by Will Cohen on 12/11/17.
+//  Copyright © 2017 Will Cohen. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class MainNewsFeed: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
