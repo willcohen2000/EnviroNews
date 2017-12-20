95f0fddfbbfa95b164e17ad9ae78f7d3a7cf0fee
willcohen2000 <willcohen2000@gmail.com>
2017-12-19 21:39:37 -0500

    Updated News Filter (better for finding exclusively environmental articles)

--- EnvironNews/Controllers/MainNewsFeed.swift
+++ EnvironNews/Controllers/MainNewsFeed.swift
@@ -22,12 +22,19 @@ class MainNewsFeed: UIViewController {
 
     private func pullNewsArticles() {
         if let apikey = App.sharedInstance.NewsAPIKey {
-            Alamofire.request("https://newsapi.org/v2/everything?q=sustainability&sortBy=popularity&language=en&apiKey=\(apikey)").responseJSON { (responseData) -> Void in
+            Alamofire.request("https://newsapi.org/v2/everything?q=environmental+technology&sortBy=popularity&language=en&apiKey=\(apikey)").responseJSON { (responseData) -> Void in
                 if((responseData.result.value) != nil) {
                     let JSONResponse = JSON(responseData.result.value!);
+                    print(JSONResponse)
                     for (subJson):(String, JSON) in JSONResponse["articles"] {
                         let response = subJson.1;
+                        let source = response["source"] as JSON;
+                        guard let sourceName = source["name"].string else { return };
+                        print(response)
+                        print("============================")
                     }
+                } else {
+                    print("computer")
                 }
             }
         }
